7462b8189b0abdc20fd7844f4a66a06509401381
Kleber Lucas.G.S. <lenovomicrosof1@gmail.com>
2026-02-14 15:22:40 -0300

    feat: add Prisma setup com inicial
    - Migração do modelo de usuário e do banco de dados
    
    - Adicionada dependência do cliente Prisma ao package.json
    
    - Criado o arquivo prisma.config.ts para configuração do Prisma
    
    - Definido o modelo de usuário inicial no schema.prisma
    
    - Adicionados arquivos de migração para criação da tabela e do índice de usuários
    
    - Introduzido o prisma.js para instância singleton do cliente Prisma
    
    - Adicionadas configurações do VSCode para o prompt do Next.js

---
+++ src/lib/prisma.js
@@ -0,0 +1,16 @@
+import { PrismaClient } from "@prisma/client";
+
+const prismaClientSingleton = () => {
+    return new PrismaClient({
+        datasources: {
+            db: { url: process.env.DATABASE_URL },
+        },
+    });
+};
+
+const globalForPrisma = global;
+const prisma = globalForPrisma.prisma || prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
